0c2fcf7f83f0fc193e81c87bb0cc3292da12c097
Luis Fernando <lcruzf@intercorp.com.pe>
2026-06-06 10:33:29 -0500

    update: script backup

--- SCRIPT_BACKUP/backup.sh
+++ SCRIPT_BACKUP/backup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # VARIABLES
+FECHA=$(date +"%Y%m%d_%H%M%S")
+
+BACKUP_NAME="backup_${FECHA}.zip"
 
 LOG_DIR="$HOME/logs"
 LOG_FILE="$LOG_DIR/backup.log"
@@ -8,14 +11,17 @@ LOG_FILE="$LOG_DIR/backup.log"
 ORIGEN=$1
 DESTINO=$2
 
+BACKUP_FILE="$DESTINO/$BACKUP_NAME"
+
 #setup
 crear_log() {
     mkdir -p "$LOG_DIR"
 }
 
 registrar_log() {
-    MENSAJE=$1
-    echo "$(date '+%Y-%m-%d %H:%M:%S') : $MENSAJE" >> "$LOG_FILE"
+    NIVEL=$1
+    MENSAJE=$2
+    echo "$(date '+%Y-%m-%d %H:%M:%S') [$NIVEL] $MENSAJE" >> "$LOG_FILE"
 }
 
 validar_parametros() {
@@ -25,10 +31,12 @@ validar_parametros() {
         echo "Error: Número de parámetros incorrecto."
         echo "Uso: $0 <origen> <destino>"
 
-        registrar_log "Error: Número de parámetros incorrecto. Uso: $0 <origen> <destino>"
+        registrar_log ERROR "Error: Número de parámetros incorrecto. Uso: $0 <origen> <destino>"
 
         exit 2
     fi
+
+    registrar_log INFO "PARAMETROS VALIDADOS CORRECTAMENTE"
 }
 
 validar_origen() {
@@ -37,51 +45,80 @@ validar_origen() {
         echo ""
         echo "Error: Directorio de origen inexistente."
 
-        registrar_log "Error: origen inexistente: $ORIGEN"
+        registrar_log ERROR "Error: origen inexistente: $ORIGEN"
 
         exit 3
     fi
+
+    registrar_log INFO "ORIGEN VALIDADO CORRECTAMENTE"
 }
 
 validar_destino() {
     if [ ! -d "$DESTINO" ]
     then
         echo ""
-        echo "ALERTA: Directorio destino inexistente."
+        registrar_log WARN "Destino inexistente: $DESTINO"
         echo "Creando directorio destino: $DESTINO"
         mkdir -p "$DESTINO"
-        registrar_log "Creando directorio destino: $DESTINO"
+        registrar_log INFO "Destino creado: $DESTINO"
+    else 
+        registrar_log INFO "Destino validado correctamente: $DESTINO"
     fi
 }
 
-crear_backup() {
-    cp -r "$ORIGEN" "$DESTINO"
-}
+verificar_espacio() {
+    ESPACIO_REQUERIDO=$(du -sb "$ORIGEN" | awk '{print $1}')
+    ESPACIO_DISPONIBLE=$(df -B1 "$DESTINO" | tail -1 | awk '{print $4}')
 
-verificar_backup() {
-    NOMBRE=$(basename "$ORIGEN")
+    registrar_log INFO "Espacio requerido: $ESPACIO_REQUERIDO"
+    registrar_log INFO "Espacio disponible: $ESPACIO_DISPONIBLE"
 
-    if [ -d "$DESTINO/$NOMBRE" ]
+    if [ "$ESPACIO_DISPONIBLE" -lt "$ESPACIO_REQUERIDO" ]
     then
         echo ""
-        echo "Backup creado exitosamente en: $DESTINO/$NOMBRE"
-        registrar_log "Backup creado exitosamente en: $DESTINO/$NOMBRE"
+        echo "ERROR: Espacio insuficiente"
+
+        registrar_log ERROR "Espacio insuficiente"
+
+        exit 4
+    fi
+    
+    registrar_log INFO "Espacio suficiente para el Backup"
+}
+
+crear_backup_zip() {
+    registrar_log INFO "Iniciando compresion"
+
+    zip -r "$BACKUP_FILE" "$ORIGEN" > /dev/null
+}
+
+verficar_backup() {
+
+    if [ -f "$BACKUP_FILE" ]
+    then
+        echo ""
+        echo "Backup generado correctamente"
+        echo "$BACKUP_FILE"
+
+        registrar_log INFO "Backup generado correctamente"
 
         exit 0
-    else
+
+    else 
         echo ""
-        echo "Error: Backup no se creó correctamente."
-        registrar_log "Error: Backup no se creó correctamente."
-        exit 4
+        echo "ERROR: Backup no generado"
+
+        registrar_log ERROR "Error generando backup"
+
+        exit 5
     fi
 }
 
 ## MAIN DE BACKUP
 
-
 crear_log
 
-registrar_log "Inicio del backup"
+registrar_log INFO "Inicio del backup"
 
 validar_parametros "$@"
 
@@ -89,6 +126,8 @@ validar_origen
 
 validar_destino
 
-crear_backup
+verificar_espacio
 
-verificar_backup
+crear_backup_zip
+
+verficar_backup
